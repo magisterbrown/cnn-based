0f1c84faffc84345677f3cad0a89e0b261e70cc9
Artur <magisterbrownie@gmail.com>
2024-08-27 17:51:44 +0200

    Learning but backwards

--- conv.c
+++ conv.c
@@ -31,6 +31,7 @@ static inline void write_image(const char* path, size_t width, size_t height, Te
         for(int x=0;x<width;x++)
             buffer[y*width+x] = 255*(tel(img, 0, img->w*x/width, img->h*y/height)-minv)/maxv;
     stbi_write_jpg(path, width, height, 1, buffer, 100);
+    printf("Image %s min: %.3f max %.3f\n", path, minv, maxv);
     free(buffer);
 }
 
@@ -88,6 +89,12 @@ float mse_loss(Tensor *input, Tensor *target, Tensor *input_grad)
     return acc/size;
 }
 
+void update(Tensor *ten, Tensor *grad)
+{
+    for(int i=0;i<tsize(ten);i++)
+        ten->data[i]+=grad->data[i]*0.0001;
+}
+
 int main(void) {
 
     FILE *mnist = fopen("./data/train-images-idx3-ubyte", "rb");
@@ -117,22 +124,31 @@ int main(void) {
 
     Tensor *conv = tcreate(((Tensor){1,3,3}));
     Tensor *lrconv = tcreate(((Tensor){1,3,3}));
+    Tensor *grad_conv= tcreate(((Tensor){1,3,3}));
     memcpy(conv->data, &((float []){-1,-2,-1,0,0,0,1,2,1}), sizeof(float)*9);
     memcpy(lrconv->data, &((float []){-0.13,0.12,0.01,0.06,-0.01,0.02,-0.1,-0.11,-0.08}), sizeof(float)*9);
-    Tensor *result = tcreate(((Tensor){1,rows,cols}));
 
-    int n = 1000;
-    double res = 0;
+    Tensor *result = tcreate(((Tensor){1,rows,cols}));
+    Tensor *lresult= tcreate(((Tensor){1,rows,cols}));
+    Tensor *loss_grad = tcreate(((Tensor){1,rows,cols}));
+
     
-    for(int i = 0; i<n; i++)
+    forward_conv(im1, conv, result);
+    
+    for(int i=0;i<10;i++)
     {
-        clock_t t = omp_get_wtime();
-        forward_conv(im1, conv, result);
-        res += omp_get_wtime()-t;
+        forward_conv(im1, lrconv, lresult);
+        float mse = mse_loss(lresult, result, loss_grad);
+        printf("Loss %.3f\n", mse);
+        backward_conv_filter(grad_conv, im1, loss_grad);
+        update(lrconv, grad_conv);
+        memset(lresult->data, 0, tsize(lresult));
     }
     
 
-    write_image("data/res.jpg", 280, 280, im1);
+    write_image("data/conv_grad.jpg", 280, 280, grad_conv);
+    write_image("data/grad.jpg", 280, 280, loss_grad);
     write_image("data/conv.jpg", 280, 280, result);
+    write_image("data/lrconv.jpg", 280, 280, lresult);
     return 0;
 }
